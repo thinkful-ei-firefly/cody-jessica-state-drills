4dd069ad5ab43a8733df6d1c05dd8b4c60a310e5
Jess Doyle <jessica.e.doyle@gmail.com>
2019-07-31 14:32:54 -0400

    add 3/4 tests

--- src/Accordion.test.js
+++ src/Accordion.test.js
@@ -2,11 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Renderer from 'react-test-renderer'
 import Accordion from './Accordion';
-import { exportAllDeclaration } from '@babel/types';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+const sections = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+]
 
 describe('<Accordion sections={sections}/>', () =>{
-  it('Renders <Accodion /> Correctly', () => {
+  it('Renders an empty list when no components are defined', () => {
     const tree = Renderer.create(<Accordion sections={[]} />).toJSON();
     expect(tree).toMatchSnapshot();
+  })
+
+  it('renders no sections as active by default', () => {
+    const tree = Renderer.create(<Accordion sections={sections} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+
+  it('renders a <p> element below the button that was clicked that displays content', () =>{
+    const wrapper = shallow(<Accordion sections={sections}/>);
+    wrapper.find('button').at(0).simulate('click');
+    expect(toJson(wrapper)).toMatchSnapshot();
   })
 })
