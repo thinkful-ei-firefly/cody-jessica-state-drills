906a9ea43da650a247534150b091c7e435c65284
Jess Doyle <jessica.e.doyle@gmail.com>
2019-07-31 12:28:30 -0400

    blank accordion files

---
+++ src/Accordion.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Accordion extends React.Component{
+  
+  
+  render() {
+      return (
+    <main className='App'>
+      {/* content goes here */}
+    </main>
+  );
+  }
+}
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import HelloWorld from './HelloWorld';
-import RouletteGun from './RouletteGun';
+import Accordion from './Accordion';
 import './index.css';
 
-ReactDOM.render(<RouletteGun bulletInChamber={8}/>, document.getElementById('root'));
+ReactDOM.render(<Accordion />, document.getElementById('root'));
